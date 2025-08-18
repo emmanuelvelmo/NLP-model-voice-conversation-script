2d67500bb7e2b8d63fc1f9245f238ca782f9e1c6
Emmanuel V. <51292782+emmanuelvelmo@users.noreply.github.com>
2025-08-17 19:56:21 -0700

    NLP model voice conversation script.py

---
+++ NLP model voice conversation script.py
@@ -0,0 +1,15 @@
+import whisper_functions # Lógica para conversión de voz a texto
+import llama_functions # Lógica para procesamiento de texto a respuesta
+import openvoice_functions # Lógica para conversión de respuesta en texto a voz
+
+# Bucle principal del programa
+while True:
+  # Esperar hasta capturar voz para convertir a texto
+  
+  
+  # Introducir texto de voz como input para generar respuesta
+  
+  
+  # Convertir respuesta a voz
+  
+  
