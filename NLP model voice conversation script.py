fe2f744dfaaf4d71844895d3676c8a552feb381e
Emmanuel V. <51292782+emmanuelvelmo@users.noreply.github.com>
2025-08-18 07:17:31 -0700

    NLP model voice conversation script.py

--- NLP model voice conversation script.py
+++ NLP model voice conversation script.py
@@ -1,15 +1,54 @@
-import whisper_functions # Lógica para conversión de voz a texto
-import llama_functions # Lógica para procesamiento de texto a respuesta
-import openvoice_functions # Lógica para conversión de respuesta en texto a voz
+import pathlib # 
+import whisper # Conversión de voz a texto
+import llama # Procesamiento de texto a respuesta
+import openvoice # Conversión de respuesta en texto a voz
+
+# FUNCIONES
+# 
+def leer_respuesta(texto_salida):
+  
+  
+
+# 
+def generar_respuesta(texto_entrada):
+  
+  
+
+# 
+def capturar_voz():
+  
+  
+
+#
+def ciclo_conversacion()
+  # Esperar hasta capturar voz para convertir a texto
+  texto_entrada = capturar_voz()
+  
+  # Introducir texto de voz como input para generar respuesta
+  texto_salida = generar_respuesta(texto_entrada)
+  
+  # Convertir respuesta a voz
+  leer_respuesta(texto_salida)
+
+# 
+def cargar_modelos():
+  # Cargar modelo NLP
+  
+  
+  # Cargar modelo Whisper
+  
+  
+
+# PUNTO DE PARTIDA
+# Solicitar colocar modelos en carpeta si no existe o no los contiene
+while True:
+  
+  
+
+# Cargar archivos de modelos NLP y Whisper
+cargar_modelo()
 
 # Bucle principal del programa
 while True:
-  # Esperar hasta capturar voz para convertir a texto
-  
-  
-  # Introducir texto de voz como input para generar respuesta
-  
-  
-  # Convertir respuesta a voz
-  
-  
+  # 
+  ciclo_conversacion()
